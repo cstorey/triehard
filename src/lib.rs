b23f429460e6074e364ea08a767a7f743018a53e
Ceri Storey <ceri@lshift.net>
2016-08-17 15:13:11 +0100

    Deletion kinda works.

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,8 @@ trait Dict<T> {
     type K;
 
     fn empty() -> Self;
-    // TODO: Collision handler fn.
     fn insert(&mut self, key: Self::K, val: T);
+    fn remove(&mut self, key: &Self::K);
     fn lookup(&self, key: &Self::K) -> Option<&T>;
 }
 
@@ -45,6 +45,29 @@ impl<T:Clone+fmt::Debug> Trie<T> {
             },
         };
         debug!("#inserted: {:?}", new);
+        new
+    }
+
+    fn del(&self, key: &u64) -> Self {
+        debug!("#delert: {:?} <- {:?}", self, key);
+        let new = match &*self {
+            &Trie::Empty => Trie::Empty,
+            &Trie::Lf(k, _) if &k == key => Trie::Empty,
+            &Trie::Lf(j, _) => self.clone(),
+            &Trie::Br(p, m, ref l, ref r) if Self::match_prefix(*key, p, m) => {
+                let leftp = Self::zerobit(*key, m);
+                debug!("zerobit({:#b}, {:#b}) => {:?}; branch:{:?}", key, m, leftp, if leftp { l } else { r });
+                if leftp {
+                    Trie::Br(p, m, Rc::new(l.del(key)), r.clone())
+                } else {
+                    Trie::Br(p, m, l.clone(), Rc::new(r.del(key)))
+                }
+            },
+            &Trie::Br(p, _, _, _) => {
+                self.clone()
+            },
+        };
+        debug!("#delerted: {:?}", new);
         new
     }
 
@@ -109,6 +132,10 @@ impl<T:Clone+fmt::Debug> Dict<T> for Trie<T> {
             }
         }
     }
+    fn remove(&mut self, key: &Self::K) {
+        let new = self.del(key);
+        *self = new;
+    }
 }
 
 #[cfg(test)]
@@ -135,5 +162,31 @@ mod tests {
             assert_eq!(res, mres);
         }
         quickcheck::quickcheck(prop_works as fn(Vec<(u64, u64)>, u64) -> ());
+    }
+
+    #[test]
+    fn should_add_remove() {
+        env_logger::init().unwrap_or(());
+        fn prop_works(insert: Vec<(u64,u64)>, remove: Vec<u64>, probe: u64) -> () {
+            debug!("{:?}", (&insert, &remove, &probe));
+            let mut d = Trie::empty();
+            let mut m = BTreeMap::new();
+            for (k,v) in insert {
+                d.insert(k, v);
+                m.insert(k, v);
+            }
+            debug!("m: {:?}; d: {:?}", m, d);
+            for k in remove {
+                d.remove(&k);
+                m.remove(&k);
+            }
+
+            let mres = m.get(&probe);
+            let res = d.lookup(&probe);
+            debug!("eq? {:?}", res == mres);
+            debug!("");
+            assert_eq!(res, mres);
+        }
+        quickcheck::quickcheck(prop_works as fn(Vec<(u64, u64)>, Vec<u64>, u64) -> ());
     }
 }
